ee94103a4a607f611f50f76fccfad243633086c7
Skye <33592926+SkyeTheDemon@users.noreply.github.com>
2021-07-24 20:13:02 -0400

    Add files via upload
    Just adding this Assignment that Hoeen gave me to do in Java.
    Inputting a integer value into the command line when running the code will tell you whether it is even or odd.

---
+++ HoeenAssignment.java
@@ -0,0 +1,26 @@
+// Instructions
+// make a program that takes a number (integer) in from command line using the Scanner object. 
+// Then check if the number is even or odd, print that info to the user eg: "5 is Odd", "8 is Even"
+
+import java.util.Scanner; 
+//Imports the Scanner class
+
+public class Main {
+    public static void Main(String[] args) 
+    {
+        int number = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a Number to check whether it is \"even\" or \"odd\""); 
+        //Reads the value entered in the command prompt
+        number = scanner.nextInt();
+
+        if((number % 2)==0) 
+        {
+            System.out.println("The Given Number\""+number+"\" is Even"); 
+        } 
+        else
+        {
+            System.out.println("The Given Number\""+number+"\" is Odd");
+        }
+    }
+}
